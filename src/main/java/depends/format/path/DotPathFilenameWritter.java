ddb8a9e3558024968aec613e317a7dafe3ad4e29
Gang ZHANG <gangz@emergentdesign.cn>
2019-02-17 08:53:22 +0800

    redesign the dot namepath replacement strategy

--- src/main/java/depends/format/path/DotPathFilenameWritter.java
+++ src/main/java/depends/format/path/DotPathFilenameWritter.java
@@ -5,11 +5,17 @@ import org.apache.commons.io.FilenameUtils;
 public class DotPathFilenameWritter implements FilenameWritter {
 	@Override
 	public String reWrite(String originalPath) {
-		String ext = FilenameUtils.getExtension(originalPath);
-		String path = replaceExt(originalPath,ext);
-		path =  path.replace('/', '.');
-		path =  path.replace('\\', '.');
-		return path;
+		String[] segments = originalPath.split("[/\\\\]");
+		StringBuilder sb = new StringBuilder();
+		for(String segment:segments) {
+			if (sb.length()>0)
+				sb.append(".");
+			sb.append(segment.replace(".", "_"));
+		}
+		if (sb.toString().startsWith("_")) {
+			return sb.toString().substring(1);
+		}
+		return sb.toString();
 	}
 
 	private String replaceExt(String path, String ext) {
@@ -17,6 +23,14 @@ public class DotPathFilenameWritter implements FilenameWritter {
 		if (ext.length()==0) return path;
 		if (!path.endsWith(ext)) return path;
 		path = path.substring(0,path.length()-ext.length()-1) + "_" + ext;
+		return path;
+	}
+	
+	public String reWriteMethod2(String originalPath) {
+		String ext = FilenameUtils.getExtension(originalPath);
+		String path = replaceExt(originalPath,ext);
+		path =  path.replace('/', '.');
+		path =  path.replace('\\', '.');
 		return path;
 	}
 }
