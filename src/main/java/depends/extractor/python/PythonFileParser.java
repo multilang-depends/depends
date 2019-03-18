3fbc714e2879967c93806f63b0d7a344ca60fb4f
Gang ZHANG <gangz@emergentdesign.cn>
2019-03-18 20:37:22 +0800

    remove debug info

--- src/main/java/depends/extractor/python/PythonFileParser.java
+++ src/main/java/depends/extractor/python/PythonFileParser.java
@@ -48,9 +48,6 @@ public class PythonFileParser implements FileParser {
 	    walker.walk(bridge, parser.file_input());
 	    
 		fileEntity = entityRepo.getEntity(fileFullPath);
-		if (fileEntity.getRawName().contains("av_bench.py")) {
-			System.out.println("hlo");
-		}
 		fileEntity.inferEntities(inferer);
 	}
 
