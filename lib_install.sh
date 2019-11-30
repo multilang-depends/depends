a074fa028bd4c65c442eb96114c272356cb39cbb
Gang ZHANG <gangz@emergentdesign.cn>
2019-11-30 10:18:17 +0800

    upgrade cdt to 6.9.0.201909091953

--- lib_install.sh
+++ lib_install.sh
@@ -1,18 +1,18 @@
 mvn install:install-file -DgroupId=org.jruby	-DartifactId=jrubyparser  -Dversion=0.5.5-SNAPSHOT    -Dpackaging=jar -Dfile=./jars/jrubyparser-0.5.5-SNAPSHOT.jar  -DgeneratePom=true
 
-mvn install:install-file -DgroupId=org.eclipse.cdt	-DartifactId=org.eclipse.cdt.core  -Dversion=6.5.0.201806170908    -Dpackaging=jar -Dfile=./jars/org.eclipse.cdt.core-6.5.0.201806170908.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=org.eclipse.cdt	-DartifactId=org.eclipse.cdt.core  -Dversion=6.9.0.201909091953    -Dpackaging=jar -Dfile=./jars/org.eclipse.cdt.core_6.9.0.201909091953.jar -DgeneratePom=true
 
-mvn install:install-file -DgroupId=eclipse-photon -DartifactId=org.eclipse.equinox.registry -Dversion=3.8.0.v20180426-1327  -Dpackaging=jar -Dfile=./jars/org.eclipse.equinox.registry-3.8.0.v20180426-1327.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=eclipse-photon -DartifactId=org.eclipse.equinox.registry -Dversion=3.8.500.v20190714-1850  -Dpackaging=jar -Dfile=./jars/org.eclipse.equinox.registry_3.8.500.v20190714-1850.jar -DgeneratePom=true
 
 mvn install:install-file -DgroupId=eclipse-photon -DartifactId=org.eclipse.core.runtime -Dversion=3.14.0.v20180417-0825  -Dpackaging=jar -Dfile=./jars/org.eclipse.core.runtime-3.14.0.v20180417-0825.jar  -DgeneratePom=true
 
-mvn install:install-file -DgroupId=eclipse-photon -DartifactId=org.eclipse.equinox.common -Dversion=3.10.0.v20180412-1130 -Dpackaging=jar -Dfile=./jars/org.eclipse.equinox.common-3.10.0.v20180412-1130.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=eclipse-photon -DartifactId=org.eclipse.equinox.common -Dversion=3.10.500.v20190815-1535 -Dpackaging=jar -Dfile=./jars/org.eclipse.equinox.common_3.10.500.v20190815-1535.jar -DgeneratePom=true
 
-mvn install:install-file -DgroupId=eclipse-photon -DartifactId=org.eclipse.osgi -Dversion=3.13.0.v20180409-1500 -Dpackaging=jar -Dfile=./jars/org.eclipse.osgi-3.13.0.v20180409-1500.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=eclipse-photon -DartifactId=org.eclipse.osgi -Dversion=3.15.0.v20190830-1434 -Dpackaging=jar -Dfile=./jars/org.eclipse.osgi_3.15.0.v20190830-1434.jar -DgeneratePom=true
 
-mvn install:install-file -DgroupId=eclipse-photon -DartifactId=org.eclipse.equinox.preferences -Dversion=3.7.100.v20180510-1129 -Dpackaging=jar -Dfile=./jars/org.eclipse.equinox.preferences-3.7.100.v20180510-1129.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=eclipse-photon -DartifactId=org.eclipse.equinox.preferences -Dversion=3.7.500.v20190815-1535 -Dpackaging=jar -Dfile=./jars/org.eclipse.equinox.preferences_3.7.500.v20190815-1535.jar -DgeneratePom=true
 
-mvn install:install-file -DgroupId=eclipse-photon -DartifactId=org.eclipse.core.jobs -Dversion=3.10.0.v20180427-1454 -Dpackaging=jar -Dfile=./jars/org.eclipse.core.jobs-3.10.0.v20180427-1454.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=eclipse-photon -DartifactId=org.eclipse.core.jobs -Dversion=3.10.500.v20190620-1426 -Dpackaging=jar -Dfile=./jars/org.eclipse.core.jobs_3.10.500.v20190620-1426.jar -DgeneratePom=true
 
-mvn install:install-file -DgroupId=eclipse-photon -DartifactId=org.eclipse.core.resources -Dversion=3.13.0.v20180512-1138 -Dpackaging=jar -Dfile=./jars/org.eclipse.core.resources_3.13.0.v20180512-1138.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=eclipse-photon -DartifactId=org.eclipse.core.resources -Dversion=3.13.500.v20190819-0800 -Dpackaging=jar -Dfile=./jars/org.eclipse.core.resources_3.13.500.v20190819-0800.jar -DgeneratePom=true
 
