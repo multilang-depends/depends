52a70558ce0b44e21e51fe9d9c602c47f7a2d7fd
Gang ZHANG <gangz@emergentdesign.cn>
2020-01-04 09:51:29 +0800

    fix package name remover

--- src/main/java/depends/entity/PackageNamePrefixRemover.java
+++ src/main/java/depends/entity/PackageNamePrefixRemover.java
@@ -4,9 +4,14 @@ public class PackageNamePrefixRemover {
 	public static String remove(Entity entity) {
 		PackageEntity pkg = (PackageEntity) entity.getAncestorOfType(PackageEntity.class);
 		String name = entity.getQualifiedName();
-		if (pkg!=null) {
-			name = name.substring(pkg.getQualifiedName().length()+1);
+		if (pkg != null) {
+			try {
+				name = name.substring(pkg.getQualifiedName().length() + 1);
+			} catch (Exception e) {
+				return name;
+			}
 		}
 		return name;
 	}
+
 }
